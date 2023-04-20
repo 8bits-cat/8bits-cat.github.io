26533010ccde6936186a2a54d73f77e24357c911
8bits-cat <x103h92@gmail.com>
2023-04-21 00:03:25 +0800

    add a md files to test

--- exmp_vplugin.sh
+++ exmp_vplugin.sh
@@ -2,7 +2,7 @@
 ## Example: a typical script with several problems
 files="$(ls ./*.m3u)"
 for f in $files 
-do
+dol
   grep -qi "hq.*mp3" "$f" \
 && echo -e "Playlist $f contains a HQ file in mp3 format"
 done
